5c5adad03b8503229a3a56c044dcebff1d119e8d
Erik <ol.erik0107@gmail.com>
2025-10-03 14:24:20 -0300

    feat: adjusting users

--- db/seed/users.ts
+++ db/seed/users.ts
@@ -1,43 +1,44 @@
 import { db } from ".."
 import { usersTable } from "../schema"
+import { PasswordHandler } from "../../src/utils/password-handler/password-handler.service"
 import { LoggerService } from "@/utils/logger/logger.service"
 import { User } from "@/types/entities"
 
+const passwordHandler = new PasswordHandler()
 const loggerService = new LoggerService()
 
 const logger = loggerService.createEntityLogger('UsersSeed')
 
-const users: Omit<User, 'id' | 'createdAt' | 'updatedAt'>[] = [
-  {
-    name: "John Doe",
-    email: "john.doe@example.com",
-    password: ("password123")
-  },
-  {
-    name: "Jane Smith",
-    email: "jane.smith@example.com",
-    password: ("password123")
-  },
-  {
-    name: "Bob Johnson",
-    email: "bob.johnson@example.com",
-    password: ("password123")
-  },
-  {
-    name: "Alice Brown",
-    email: "alice.brown@example.com",
-    password: ("password123")
-  },
-  {
-    name: "Charlie Wilson",
-    email: "charlie.wilson@example.com",
-    password: ("password123")
-  }
-]
-
 export const usersSeed = async () => {
   const startTime = Date.now()
 
+  const users: Omit<User, 'id' | 'createdAt' | 'updatedAt'>[] = [
+    {
+      name: "John Doe",
+      email: "john.doe@example.com",
+      password: await passwordHandler.hashPassword("password123")
+    },
+    {
+      name: "Jane Smith",
+      email: "jane.smith@example.com",
+      password: await passwordHandler.hashPassword("password123")
+    },
+    {
+      name: "Bob Johnson",
+      email: "bob.johnson@example.com",
+      password: await passwordHandler.hashPassword("password123")
+    },
+    {
+      name: "Alice Brown",
+      email: "alice.brown@example.com",
+      password: await passwordHandler.hashPassword("password123")
+    },
+    {
+      name: "Charlie Wilson",
+      email: "charlie.wilson@example.com",
+      password: await passwordHandler.hashPassword("password123")
+    }
+  ]
 
   logger.info(`🌱 Starting to seed ${users.length} users...`)
 
