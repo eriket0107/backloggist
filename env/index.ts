1ecfeb8f88c3c90f958b0cca440a2f1c08c6f355
Erik <ol.erik0107@gmail.com>
2026-06-08 20:15:43 -0300

    feat(config): enhance database environment configuration and defaults

--- env/index.ts
+++ env/index.ts
@@ -2,16 +2,23 @@ import "dotenv/config";
 
 import z from "zod";
 
+const generateDatabaseUrl = () => {
+  return `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
+};
+
+
+
 const envSchema = z.object({
   NODE_ENV: z.enum(["dev", "prod", "test"]).default("dev"),
   JWT_SECRET: z.string().optional().default("devTest"),
   PORT: z.coerce.number().default(3333),
   HOST: z.string().default("localhost"),
-  DATABASE_URL: z.string(),
+  DATABASE_URL: z.string().default(() => generateDatabaseUrl()),
+  POSTGRES_HOST: z.string().default("localhost"),
   POSTGRES_USER: z.string(),
   POSTGRES_PASSWORD: z.string(),
   POSTGRES_DB: z.string(),
-  POSTGRES_PORT: z.coerce.number(),
+  POSTGRES_PORT: z.coerce.number().optional().default(5432),
   UPLOADS_DIR: z.string().default('src/uploads'),
   LOGS_DIR: z.string().default('logs'),
   LOG_USERNAME: z.string().default('admin'),
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
+import { env } from '@/env';
 
 
 async function bootstrap() {
@@ -36,6 +37,7 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: http://localhost:${port}`);
+  console.log("DATABASE_URL", env.DATABASE_URL);
 }
 
 bootstrap();
