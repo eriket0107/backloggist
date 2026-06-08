99dfb392cac1af9b6439f6db609f9bdf9316935c
Erik <ol.erik0107@gmail.com>
2026-06-08 20:16:07 -0300

    feat(config): only log DATABASE_URL in non-prod environments

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,9 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: http://localhost:${port}`);
-  console.log("DATABASE_URL", env.DATABASE_URL);
+  if (env.NODE_ENV !== "prod") {
+    console.log("DATABASE_URL", env.DATABASE_URL);
+  }
 }
 
 bootstrap();
